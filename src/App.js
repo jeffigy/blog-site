91d16b2e5b646f8c4d858af88e9e68dec84c748a
jeffigy <evangelistajeffy25@gmail.com>
2023-01-27 10:27:52 +0800

    6th commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 
 import Navbar from 'components/Navbar'
+import BlogDetails from 'pages/BlogDetails';
 import Create from 'pages/Create';
 import Home from 'pages/Home'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
@@ -11,8 +12,9 @@ function App() {
         <Navbar />
         <div className="content">
           <Routes>
-            <Route exact path='/' element={<Home />}/>
-            <Route exact path='/create' element={<Create/>}/>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/create' element={<Create />} />
+            <Route exact path='/blogs/:id' element={<BlogDetails />} />
           </Routes>
         </div>
       </div>
